8212113796bcc7260026e4c4d689b436a5e36758
amesemyta1 <amesemyta@gmail.com>
2022-05-13 18:27:15 +0300

    Js

---
+++ JS_Lessons/lesson1/index.js
@@ -0,0 +1,19 @@
+console.log("Hello, world!");
+
+const userName = "John";
+const greeting = "I'm John";
+const hobby = "Walking";
+const avouriteDessert = "cake";
+const superNumber = 17;
+
+console.log(userName, greeting, hobby, favouriteDesert, superNumber);
+
+let a = 4;
+let b = 2;
+
+console.log("a: ", a);
+console.log("b: ", b);
+console.log("a - b = ", a - b);
+console.log("a + b = ", a + b);
+console.log("a / b = ", a / b);
+console.log("a * b = ", a * b);
