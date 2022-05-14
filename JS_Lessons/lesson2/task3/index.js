2a723cce5d08221d66d0d8c3ab5afd232db80ba8
amesemyta1 <amesemyta@gmail.com>
2022-05-14 14:09:05 +0300

    task 2 3

--- JS_Lessons/lesson2/task3/index.js
+++ JS_Lessons/lesson2/task3/index.js
@@ -1,70 +1,69 @@
 /* eslint-disable */
 
-// Унарный плюс приводит операнд к числу
-+17;
-+"77";
+/* Результатом сравнения является булевое значение */
 
-console.log("+17:", +17);
-console.log("+'77':", +"77");
-
-// Бинарный плюс складывает строки или прибавляет числа
-10 + "5";
-"some" + "text";
-
-console.log("10 + '5':", 10 + "5");
-console.log("'some' + 'text':", "some" + "text");
-
-// Математические операции с undefined возвращают NaN
-undefined + 1;
-null + 8;
-undefined + "5";
-7 - null;
-5 - undefined;
-"text" + null;
-
-console.log("undefined + 1:", undefined + 1);
-console.log("null + 8:", null + 8);
-console.log("undefined + '5'", undefined + "5");
-console.log("7 - null:", 7 - null);
-console.log("5 - undefined:", 5 - undefined);
-console.log("'text' + null:", "text" + null);
-
-// Постфиксная и префиксная форма записи
-let num = 17;
-++num;
-num++;
-
-console.log("num:", num);
-console.log("++num:", ++num);
-console.log("num++:", num++);
-
-// математические операторы приводят операнды к числам
+/* Сравнение чисел */
 const a = 17;
-const b = -a;
-"24" / 4;
-18 * "3";
-"9" - "3";
-"seventeen" / 3;
-4 * "four";
-17 / 0;
+const b = 5;
+a > b;
+console.log("a > b:", a > b);
+a <= b;
+console.log("a <= b:", a <= b);
+a == b;
+console.log("a == b:", a == b);
+a != b;
+console.log("a != b:", a != b);
 
-console.log("a:", a);
-console.log("b:", b);
-console.log("'24' / 4:", "24" / 4);
-console.log("18 * '3':", 18 * "3");
-console.log("'9' - '3':", "9" - "3");
-console.log("'seventeen' / 3:", "seventeen" / 3);
-console.log("4 * 'four':", 4 * "four");
-console.log("17 / 0:", 17 / 0);
+/* Сравнение строк */
+"a" < "b";
+console.log("'a' < 'b':", "a" < "b");
+"ab" > "a";
+console.log("'ab' > 'a':", "ab" > "a");
 
-10 + "seven";
-undefined + 1;
-null + 8;
-undefined + "5";
-"text" + null;
+/* При сравнении переменных разных типов они преобразуются в числа */
+"17" > 1;
+console.log("'17' > 1:", "17" > 1);
 
-console.log("10 + 'seven':", 10 + "seven");
-console.log("undefined + 1:", undefined + 1);
-console.log("null + 8:", null + 8);
-console.log("undefined + '5':", undefined + "5");
-console.log("'text' + null:", "text" + null);
+/* Строгое сравнение */
+17 === 1;
+console.log("17 === 1:", 17 === 1);
+17 === 17;
+console.log("17 === 17:", 17 === 17);
+17 === "17";
+console.log("17 === '17':", 17 === "17");
+17 === true;
+console.log("17 === true:", 17 === true);
+"0" === "";
+console.log("'0' === '':", "0" === "");
+true === false;
+console.log("true === false:", true === false);
+true === true;
+console.log("true === true:", true === true);
+null === undefined;
+console.log("null === undefined", null === undefined);
+false === 0;
+console.log("false === 0:", false === 0);
+
+/* Нестрогое сравнение */
+17 == "17";
+console.log("17 == '17':", 17 == "17");
+"0" == "";
+console.log("'0' == '':", "0" == "");
+0 == "";
+console.log("0 == '':", 0 == "");
+null == undefined;
+console.log("null == undefined:", null == undefined);
+false == 0;
+console.log("false == 0:", false == 0);
+
+/* Осторожно c null и undefined */
+undefined == null;
+console.log("undefined == null:", undefined == null);
+undefined == 0;
+console.log("undefined == 0:", undefined == 0);
+null == 0;
+console.log("null == 0:", null == 0);
+undefined < 0;
+console.log("undefined < 0:", undefined < 0);
+undefined > 0;
+console.log("undefined > 0:", undefined > 0);
