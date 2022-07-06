c967549d800c08982f36cfcb1df564bf34340595
amesemyta1 <amesemyta@gmail.com>
2022-07-06 21:16:06 +0300

    les 19 -20

---
+++ JS_Lessons/lesson20/task1/index.js
@@ -0,0 +1,68 @@
+class User {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  sayHi() {
+    console.log(`Hi, I am ${this.name}`);
+  }
+
+  requestNewPhoto() {
+    console.log(`New photo request was sent for ${this.name}`);
+  }
+
+  setAge(age) {
+    if (age < 0) {
+      return false;
+    }
+    this.age = age;
+    if (age >= 25) {
+      this.age = age;
+      console.log(`New photo request was sent for ${this.name}`);
+    }
+    return age;
+  }
+
+  static createEmpty() {
+    const newUser = new User('', null);
+    return newUser;
+  }
+}
+
+// function User(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
+
+// User.prototype.sayHi = function () {
+//   console.log(`Hi, I am ${this.name}`);
+// };
+
+// User.prototype.requestNewPhoto = function () {
+//   console.log(`New photo request was sent for ${this.name}`);
+// };
+
+// // eslint-disable-next-line consistent-return
+// User.prototype.setAge = function (age) {
+//   if (age < 0) {
+//     return false;
+//   }
+//   this.age = age;
+//   if (age >= 25) {
+//     this.age = age;
+//     console.log(`New photo request was sent for ${this.name}`);
+//   }
+//   if (age > 0) {
+//     return age;
+//   }
+// };
+
+const user1 = new User('Tom', 17);
+const user2 = new User('Bob', 21);
+const user3 = new User('Sara', 36);
+
+console.log(user1.sayHi());
+console.log(user2.requestNewPhoto());
+console.log(user2.setAge(27));
+console.log(user2);
---
+++ JS_Lessons/lesson20/task2/index.js
@@ -0,0 +1,36 @@
+// eslint-disable-next-line max-classes-per-file
+class Vehicle {
+  constructor(name, numberOfWheels) {
+    this.numberOfWheels = numberOfWheels;
+    this.name = name;
+  }
+
+  move() {
+    console.log(`${this.name} is moving`);
+  }
+
+  stop() {
+    console.log(`${this.name} stopped`);
+  }
+}
+
+class Ship extends Vehicle {
+  constructor(name, numberOfWheels, maxSpeed) {
+    super(name, numberOfWheels);
+    this.maxSpeed = maxSpeed;
+  }
+
+  move() {
+    console.log(`${this.name} lifting anchor up`);
+    super.move();
+  }
+
+  stop() {
+    super.stop();
+    console.log(`${this.name} lifting anchor down`);
+  }
+}
+
+const ship1 = new Ship('Aurora', 15);
+
+console.log(ship1);
