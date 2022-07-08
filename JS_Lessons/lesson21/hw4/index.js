d1680632c405f5832df12311e9ffe628e98c8c6d
amesemyta1 <amesemyta@gmail.com>
2022-07-08 15:52:47 +0300

    hw4

--- JS_Lessons/lesson21/hw4/index.js
+++ JS_Lessons/lesson21/hw4/index.js
@@ -1,22 +1,4 @@
-class Wallet {
-  constructor() {
-    this._balance = 0;
-  }
-
-  getBalance() {
-    return this._balance;
-  }
-
-  deposit(amount) {
-    this._balance += amount;
-  }
-
-  withdraw(amount) {
-    if (amount > this._balance) {
-      console.log('No enough funds');
-      return;
-    }
-
-    this._balance -= amount;
-  }
+export function setTitle(text) {
+  const title = document.querySelector('.title');
+  title.textContent = text;
 }
