9eb71223fb66008ec9c683f881b106dc30e9b8aa
amesemyta1 <amesemyta@gmail.com>
2022-07-08 20:06:53 +0300

    hw11

--- JS_Lessons/lesson21/hw11/index.js
+++ JS_Lessons/lesson21/hw11/index.js
@@ -1,4 +1,6 @@
-export function setTitle(text) {
-  const title = document.querySelector('.title');
-  title.textContent = text;
+export function squaredNumbers() {
+  const element = document.querySelector('.number');
+  const squareElement = element.dataset.number * element.dataset.number;
+  console.log(squareElement);
+  element.dataset.squaredNumber = squareElement;
 }
