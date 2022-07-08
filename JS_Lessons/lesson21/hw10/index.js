13db1377d132915d45d3efb4bb2958f24df3bf8f
amesemyta1 <amesemyta@gmail.com>
2022-07-08 19:52:25 +0300

    hw10

--- JS_Lessons/lesson21/hw10/index.js
+++ JS_Lessons/lesson21/hw10/index.js
@@ -1,4 +1,15 @@
-export function setTitle(text) {
-  const title = document.querySelector('.title');
-  title.textContent = text;
+export function manageClasses() {
+  const elementOne = document.querySelector('.one');
+  elementOne.classList.add('selected');
+
+  const elementTwo = document.querySelector('.two');
+  elementTwo.classList.remove('selected');
+
+  const elementThree = document.querySelector('.three');
+  elementThree.classList.toggle('three_done');
+
+  const elementFour = document.querySelector('.four');
+  if (elementFour.classList.contains('some-class')) {
+    elementFour.classList.add('another-class');
+  }
 }
