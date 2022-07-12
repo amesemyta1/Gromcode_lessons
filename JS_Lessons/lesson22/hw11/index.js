7969892c7df0342869b8ccc9d60ce6d318931a8d
amesemyta1 <amesemyta@gmail.com>
2022-07-12 18:44:13 +0300

    ls22

--- JS_Lessons/lesson22/hw11/index.js
+++
@@ -1,8 +0,0 @@
-export function squaredNumbers() {
-  const element = document.querySelectorAll('.number');
-  for (let i = 0; i < element.length; i += 1) {
-    const numberElement = element[i].dataset.number;
-    const squaredElement = numberElement * numberElement;
-    element[i].dataset.squaredNumber = squaredElement;
-  }
-}
