e5a83c2104e0a1f2833db14807b6e018eb546ff5
amesemyta1 <amesemyta@gmail.com>
2022-07-12 18:24:44 +0300

    hw8

--- JS_Lessons/lesson22/hw8/index.js
+++ JS_Lessons/lesson22/hw8/index.js
@@ -1,16 +1,7 @@
-export function finishList() {
-  const element1 = document.createElement('li');
-  const element8 = document.createElement('li');
-  const element4 = document.createElement('li');
-  const element6 = document.createElement('li');
-  const list = document.querySelector('.list');
-  const special = document.querySelector('.special');
-  element1.textContent = '1';
-  list.prepend(element1);
-  element8.textContent = '8';
-  list.append(element8);
-  element4.textContent = '4';
-  special.before(element4);
-  element6.textContent = '6';
-  special.after(element6);
+const checkboxElem = document.querySelector('.task-status');
+
+function consoleChecked(event) {
+  console.log(event.target.checked);
 }
+
+checkboxElem.addEventListener('change', consoleChecked);
