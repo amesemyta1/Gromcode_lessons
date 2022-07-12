86b362d1fcd9cc8ace77eb02611d1d8c4e637fa2
amesemyta1 <amesemyta@gmail.com>
2022-07-12 17:07:10 +0300

    hw3

--- JS_Lessons/lesson22/hw3/index.js
+++ JS_Lessons/lesson22/hw3/index.js
@@ -1,23 +1,8 @@
-export function getTitle() {
-  const text = document.querySelector('.title');
-  console.log(text.textContent);
-  return text.textContent;
+const checkboxElem = document.querySelector('.task-status');
+
+function consoleValue() {
+  console.log(checkboxElem.checked);
+  console.log(checkboxElem.value);
 }
 
-export function getDescription() {
-  const text = document.querySelector('.about');
-  console.log(text.innerText);
-  return text.innerText;
-}
-
-export function getPlans() {
-  const text = document.querySelector('.plans');
-  console.log(text.innerHTML);
-  return text.innerHTML;
-}
-
-export function getGoal() {
-  const text = document.querySelector('.goal');
-  console.log(text.outerHTML);
-  return text.outerHTML;
-}
+checkboxElem.addEventListener('change', consoleValue);
