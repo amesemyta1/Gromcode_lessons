2a6ed43a03c65e35bed4054aff3d2562bf9bd014
amesemyta1 <amesemyta@gmail.com>
2022-07-12 17:11:45 +0300

    hw4

--- JS_Lessons/lesson22/hw4/index.js
+++ JS_Lessons/lesson22/hw4/index.js
@@ -1,4 +1,8 @@
-export function setTitle(text) {
-  const title = document.querySelector('.title');
-  title.textContent = text;
+const btnElem = document.querySelector('.search__btn');
+const inputElem = document.querySelector('.search__input');
+
+function inputText() {
+  console.log(inputElem.value);
 }
+
+btnElem.addEventListener('click', inputText);
