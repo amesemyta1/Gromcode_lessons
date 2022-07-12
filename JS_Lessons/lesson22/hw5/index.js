10a2861a57d51231cae188b7eb0e7053bf9f7edf
amesemyta1 <amesemyta@gmail.com>
2022-07-12 17:48:33 +0300

    hw5

--- JS_Lessons/lesson22/hw5/index.js
+++ JS_Lessons/lesson22/hw5/index.js
@@ -1,3 +1,8 @@
-export function setButton(buttonText) {
-  document.querySelector('body').innerHTML = `<button>${buttonText}</button>`;
+const buttonElem = document.querySelector('.single-use-btn');
+
+function handleClick() {
+  console.log('clicked');
+  buttonElem.removeEventListener('click', handleClick);
 }
+
+buttonElem.addEventListener('click', handleClick);
