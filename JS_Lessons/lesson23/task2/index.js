ae776f26ed8a09c9047d6ef3845bc971ee1e4c32
amesemyta1 <amesemyta@gmail.com>
2022-07-13 16:40:53 +0300

    tsk2

--- JS_Lessons/lesson23/task2/index.js
+++ JS_Lessons/lesson23/task2/index.js
@@ -1,32 +1,74 @@
-/* eslint-disable max-classes-per-file */
-class Vehicle {
-  constructor(name, numberOfWheels) {
-    this.numberOfWheels = numberOfWheels;
-    this.name = name;
+const generateNumbersRange = (from, to) => {
+  const result = [];
+
+  for (let i = from; i <= to; i += 1) {
+    result.push(i);
   }
 
-  move() {
-    console.log(`${this.name} is moving`);
+  return result;
+};
+
+const getLineSeats = () =>
+  generateNumbersRange(1, 10)
+    .map(
+      seatNumber =>
+        `<div
+            class="sector__seat"
+            data-seat-number="${seatNumber}"
+        ></div>
+    `,
+    )
+    .join('');
+
+const getSectorLines = () => {
+  const seatsString = getLineSeats();
+
+  return generateNumbersRange(1, 10)
+    .map(
+      lineNumber =>
+        `<div
+            class="sector__line"
+            data-line-number="${lineNumber}"
+        >${seatsString}</div>`,
+    )
+    .join('');
+};
+
+const arenaElem = document.querySelector('.arena');
+
+const renderArena = () => {
+  const linesString = getSectorLines();
+
+  const sectorsString = generateNumbersRange(1, 3)
+    .map(
+      sectorNumber =>
+        `<div
+                class="sector"
+                data-sector-number="${sectorNumber}"
+            >${linesString}</div>
+        `,
+    )
+    .join('');
+
+  arenaElem.innerHTML = sectorsString;
+};
+
+const onSeatSelect = event => {
+  const isSeat = event.target.classList.contains('sector__seat');
+
+  if (!isSeat) {
+    return;
   }
 
-  stop() {
-    console.log(`${this.name} stopped`);
-  }
-}
+  const { seatNumber } = event.target.dataset;
+  const { lineNumber } = event.target.closest('.sector__line').dataset;
+  const { sectorNumber } = event.target.closest('.sector').dataset;
 
-class Ship extends Vehicle {
-  constructor(name, numberOfWheels, maxSpeed) {
-    super(name, numberOfWheels);
-    this.maxSpeed = maxSpeed;
-  }
+  const selectedSeatElem = document.querySelector('.board__selected-seat');
 
-  move() {
-    console.log(`${this.name} lifting anchor up`);
-    super.move();
-  }
+  selectedSeatElem.textContent = `S ${sectorNumber} - L ${lineNumber} - S ${seatNumber}`;
+};
 
-  stop() {
-    super.stop();
-    console.log(`${this.name} lifting anchor down`);
-  }
-}
+arenaElem.addEventListener('click', onSeatSelect);
+
+renderArena();
