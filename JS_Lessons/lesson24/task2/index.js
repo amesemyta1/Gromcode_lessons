8088aaabd9c23f04e86507eaf61d5c3b63af1448
amesemyta1 <amesemyta@gmail.com>
2022-07-14 15:21:23 +0300

    tsk2

--- JS_Lessons/lesson24/task2/index.js
+++ JS_Lessons/lesson24/task2/index.js
@@ -1,74 +1,10 @@
-const generateNumbersRange = (from, to) => {
-  const result = [];
+const formatter = new Intl.DateTimeFormat('en', {
+  timeZone: 'UTC',
+  hour: '2-digit',
+  minute: '2-digit',
+  hour12: false,
+});
 
-  for (let i = from; i <= to; i += 1) {
-    result.push(i);
-  }
+const getGreenwichTime = date => formatter.format(date);
 
-  return result;
-};
-
-const getLineSeats = () =>
-  generateNumbersRange(1, 10)
-    .map(
-      seatNumber =>
-        `<div
-            class="sector__seat"
-            data-seat-number="${seatNumber}"
-        ></div>
-    `,
-    )
-    .join('');
-
-const getSectorLines = () => {
-  const seatsString = getLineSeats();
-
-  return generateNumbersRange(1, 10)
-    .map(
-      lineNumber =>
-        `<div
-            class="sector__line"
-            data-line-number="${lineNumber}"
-        >${seatsString}</div>`,
-    )
-    .join('');
-};
-
-const arenaElem = document.querySelector('.arena');
-
-const renderArena = () => {
-  const linesString = getSectorLines();
-
-  const sectorsString = generateNumbersRange(1, 3)
-    .map(
-      sectorNumber =>
-        `<div
-                class="sector"
-                data-sector-number="${sectorNumber}"
-            >${linesString}</div>
-        `,
-    )
-    .join('');
-
-  arenaElem.innerHTML = sectorsString;
-};
-
-const onSeatSelect = event => {
-  const isSeat = event.target.classList.contains('sector__seat');
-
-  if (!isSeat) {
-    return;
-  }
-
-  const { seatNumber } = event.target.dataset;
-  const { lineNumber } = event.target.closest('.sector__line').dataset;
-  const { sectorNumber } = event.target.closest('.sector').dataset;
-
-  const selectedSeatElem = document.querySelector('.board__selected-seat');
-
-  selectedSeatElem.textContent = `S ${sectorNumber} - L ${lineNumber} - S ${seatNumber}`;
-};
-
-arenaElem.addEventListener('click', onSeatSelect);
-
-renderArena();
+console.log(getGreenwichTime(new Date()));
