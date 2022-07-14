221fee5aff96c33a78c579675a540f72d8fae146
amesemyta1 <amesemyta@gmail.com>
2022-07-14 15:10:10 +0300

    day

--- JS_Lessons/lesson24/task1/index.js
+++ JS_Lessons/lesson24/task1/index.js
@@ -1,12 +1,12 @@
 const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
 
-export const daysOfWeek = (date, days) => {
+export const dayOfWeek = (date, days) => {
   const day = new Date(date).getDate();
   const dateInFuture = new Date(date).setDate(day + days);
 
   return weekDays[new Date(dateInFuture).getDay()];
 };
 
-const result = daysOfWeek(new Date(2019, 0, 1), 14);
+const result = dayOfWeek(new Date(2019, 0, 1), 14);
 
 console.log(result);
