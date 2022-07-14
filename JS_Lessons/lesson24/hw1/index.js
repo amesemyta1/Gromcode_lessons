5a541f032bf188975140951de2e4f5693b451923
amesemyta1 <amesemyta@gmail.com>
2022-07-14 17:31:13 +0300

    hw1

--- JS_Lessons/lesson24/hw1/index.js
+++ JS_Lessons/lesson24/hw1/index.js
@@ -1,10 +1,10 @@
 export const getDiff = (startDate, endDate) => {
-  const result = endDate.getTime() - startDate.getTime();
+  const diff = Math.abs(Math.floor(endDate.getTime() - startDate.getTime()));
   console.log('start: ', startDate, '\nend: ', endDate);
-  const days = Math.ceil(result / (1000 * 60 * 60 * 24));
-  const hours = Math.ceil(result / (1000 * 60 * 60));
-  const minutes = Math.ceil(result / (1000 * 60));
-  const seconds = Math.ceil(result / 1000);
+  const days = parseInt(diff / (1000 * 60 * 60 * 24), 10);
+  const hours = parseInt((diff / (1000 * 60 * 60)) % 24, 10);
+  const minutes = parseInt((diff / (1000 * 60)) % 60, 10);
+  const seconds = parseInt((diff / 1000) % 60, 10);
   console.log(`${days}d ${hours}h ${minutes}m ${seconds}s`);
 
   return `${days}d ${hours}h ${minutes}m ${seconds}s`;
