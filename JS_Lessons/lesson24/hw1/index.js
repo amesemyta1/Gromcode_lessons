f2567b0d8425eb79a66cb6eac59f5ff1997ef03b
amesemyta1 <amesemyta@gmail.com>
2022-07-14 16:25:56 +0300

    hw1

--- JS_Lessons/lesson24/hw1/index.js
+++ JS_Lessons/lesson24/hw1/index.js
@@ -1,77 +1,15 @@
-const tasks = [
-  { text: 'Buy milk', done: false },
-  { text: 'Pick up Tom from airport', done: false },
-  { text: 'Visit party', done: false },
-  { text: 'Visit doctor', done: true },
-  { text: 'Buy meat', done: true },
-];
+export const getDiff = (startDate, endDate) => {
+  const result = endDate.getTime() - startDate.getTime();
+  console.log('start: ', startDate, '\nend: ', endDate);
+  const days = Math.ceil(result / (1000 * 60 * 60 * 24));
+  const hours = Math.ceil(result / (1000 * 60 * 60));
+  const minutes = Math.ceil(result / (1000 * 60));
+  const seconds = Math.ceil(result / 1000);
+  console.log(`${days}d ${hours}h ${minutes}m ${seconds}s`);
 
-const listElem = document.querySelector('.list');
-
-// renderTasks
-
-const renderTasks = tasksList => {
-  const tasksElems = tasksList
-    .sort((a, b) => a.done - b.done)
-    .map(({ text, done }, index) => {
-      const listItemElem = document.createElement('li');
-      listItemElem.classList.add('list__item');
-      const checkbox = document.createElement('input');
-
-      checkbox.setAttribute('type', 'checkbox');
-      checkbox.dataset.id = index;
-      checkbox.checked = done;
-
-      checkbox.classList.add('list__item-checkbox');
-      if (done) {
-        listItemElem.classList.add('list__item_done');
-      }
-
-      listItemElem.append(checkbox, text);
-
-      return listItemElem;
-    });
-
-  listElem.append(...tasksElems);
+  return `${days}d ${hours}h ${minutes}m ${seconds}s`;
 };
 
-renderTasks(tasks);
+const test = getDiff(new Date(2022, 6, 1), new Date());
 
-// createNewTask
-
-const createNewTask = () => {
-  const taskInputField = document.querySelector('.task-input');
-  const inputFieldValue = taskInputField.value;
-  if (inputFieldValue === '') {
-    return;
-  }
-  const newTask = {
-    text: inputFieldValue,
-    done: false,
-  };
-  tasks.push(newTask);
-  taskInputField.value = '';
-  taskInputField.innerHTML = '';
-  listElem.innerHTML = '';
-  renderTasks(tasks);
-};
-
-const createTaskBtnElem = document.querySelector('.create-task-btn');
-
-createTaskBtnElem.addEventListener('click', createNewTask);
-
-// changeStatus
-
-const changeStatus = event => {
-  const idNum = event.target.dataset.id;
-
-  if (tasks[idNum].done) {
-    tasks[idNum].done = false;
-  } else {
-    tasks[idNum].done = true;
-  }
-  listElem.innerHTML = '';
-  renderTasks(tasks);
-};
-
-listElem.addEventListener('click', changeStatus);
+console.log(test);
