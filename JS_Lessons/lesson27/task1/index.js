31b15035e9f3b1b586370506f2f104b8aed453c1
amesemyta1 <amesemyta@gmail.com>
2022-07-15 15:22:16 +0300

    ls 27 tsk1

---
+++ JS_Lessons/lesson27/task1/index.js
@@ -0,0 +1,13 @@
+localStorage.setItem('hobbies', JSON.stringify({ name: 'Tom' }));
+
+const getLocalStorageData = () => {
+  return Object.entries(localStorage).reduce((acc, [key, value]) => {
+    let newValue;
+    try {
+      newValue = JSON.parse(value);
+    } catch (e) {
+      newValue = value;
+    }
+    return { ...acc, [key]: newValue };
+  }, {});
+};
