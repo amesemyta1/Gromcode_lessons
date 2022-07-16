de07aa95a467c1a9e14473b14e3f485dee0415f8
amesemyta1 <amesemyta@gmail.com>
2022-07-16 16:01:49 +0300

    tsk2

--- JS_Lessons/lesson28/task2/index.js
+++ JS_Lessons/lesson28/task2/index.js
@@ -1,8 +1,37 @@
-const formatter = new Intl.DateTimeFormat('en', {
-  timeZone: 'UTC',
-  hour: '2-digit',
-  minute: '2-digit',
-  hour12: false,
-});
+export const markFavorites = (tree, favorites) => {
+  const isFavorite = favorites.includes(tree.id);
 
-export const getGreenwichTime = date => formatter.format(date);
+  return {
+    ...tree,
+    isFavorite,
+    nodes: tree.nodes.map(childNode => markFavorites(childNode, favorites)),
+  };
+};
+
+// const favorites = ['id-2'];
+// const tree = {
+//     id: 'id-1',
+//     name: 'Products',
+//     nodes: [
+//         {
+//             id: 'id-2',
+//             name: 'Food',
+//             nodes: []
+//         },
+//     ],
+// };
+
+// const result = markFavorites(tree, favorites);
+// result; // {
+//     id: 'id-1',
+//     name: 'Products',
+//     isFavorite: false,
+//     nodes: [
+//         {
+//             id: 'id-2',
+//             isFavorite: true,
+//             name: 'Food',
+//             nodes: []
+//         },
+//     ],
+// };
