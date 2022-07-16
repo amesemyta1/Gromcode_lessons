b711b9dc96ee6a50513ce016c9e6fd01b30f9977
amesemyta1 <amesemyta@gmail.com>
2022-07-16 16:54:23 +0300

    tsk3

--- JS_Lessons/lesson28/task3/index.js
+++ JS_Lessons/lesson28/task3/index.js
@@ -1,12 +1,27 @@
 // const sum1 = (a, b) => a + b;
 
-export const mult =
-  a =>
-  (b = a) =>
-    a * b;
+// const mult = a => b => a * b;
 
-export const twice = a => mult(a);
+// const twice = a => mult(a) + a;
 
-export const triple = a => mult(a) * a;
+// const triple = a => mult(a) * a;
 
-// const test = mult(5)(4);
+export function mult(a) {
+  return b => {
+    return a * b;
+  };
+}
+
+export function twice(a) {
+  return mult(a)(2);
+}
+
+export function triple(a) {
+  return mult(a)(3);
+}
+
+console.log(mult(5)(4));
+
+console.log(twice(7));
+
+console.log(triple(9));
