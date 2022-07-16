b073b1d5ae4ef7abb8b0415f7854d527580b925d
amesemyta1 <amesemyta@gmail.com>
2022-07-16 16:13:25 +0300

    tsk3

--- JS_Lessons/lesson28/task3/index.js
+++ JS_Lessons/lesson28/task3/index.js
@@ -2,8 +2,8 @@
 
 export const mult = a => b => a * b;
 
-export const twice = a => a * a;
+export const twice = mult(7);
 
-export const triple = a => a * a * a;
+export const triple = twice(9);
 
 // const test = mult(5)(4);
