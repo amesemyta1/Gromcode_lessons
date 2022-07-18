e98c5b21e9dd5e2127053d14f8649546febca1ae
amesemyta1 <amesemyta@gmail.com>
2022-07-18 17:20:49 +0300

    ls 30

--- JS_Lessons/lesson29/task1/index.js
+++ JS_Lessons/lesson29/task1/index.js
@@ -1,33 +1,38 @@
 // add image
 
-export const addImage = (imgSrc, callback) => {
-  const imgElem = document.createElement('img');
-  imgElem.setAttribute('alt', 'My Photo');
-  imgElem.src = imgSrc;
-  const containerElem = document.querySelector('.page');
-  containerElem.append(imgElem);
+export const addImage = imgSrc => {
+  const p = new Promise((resolve, reject) => {
+    const imgElem = document.createElement('img');
+    imgElem.setAttribute('alt', 'My Photo');
+    imgElem.src = imgSrc;
+    const containerElem = document.querySelector('.page');
+    containerElem.append(imgElem);
 
-  const onImageLoad = () => {
-    callback(null, imgElem);
-  };
+    const onImageLoad = () => {
+      const { width, height } = imgElem;
+      resolve({ width, height });
+    };
 
-  imgElem.addEventListener('load', onImageLoad);
+    imgElem.addEventListener('load', onImageLoad);
 
-  imgElem.addEventListener('error', () => callback('Image load is failed'));
+    imgElem.addEventListener('error', () => reject(new Error('Image load is failed')));
+  });
+
+  return p;
 };
 
 // callback function
-const onImageLoaded = (error, imgElem) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
+// const onImageLoaded = (error, imgElem) => {
+//   if (error) {
+//     console.log(error);
+//     return;
+//   }
 
-  const { width, height } = imgElem;
-  const sizeElem = document.querySelector('.image-size');
+//   const { width, height } = imgElem;
+//   const sizeElem = document.querySelector('.image-size');
 
-  sizeElem.textContent = `${width} x ${height}`;
-};
+//   sizeElem.textContent = `${width} x ${height}`;
+// };
 
 // examples
 // addImage('https://proza.ru/pics/2019/02/05/56.jpg', onImageLoaded);
---
+++ JS_Lessons/lesson30/task1/index.js
@@ -0,0 +1,33 @@
+// add image
+
+export const addImage = (imgSrc, callback) => {
+  const imgElem = document.createElement('img');
+  imgElem.setAttribute('alt', 'My Photo');
+  imgElem.src = imgSrc;
+  const containerElem = document.querySelector('.page');
+  containerElem.append(imgElem);
+
+  const onImageLoad = () => {
+    callback(null, imgElem);
+  };
+
+  imgElem.addEventListener('load', onImageLoad);
+
+  imgElem.addEventListener('error', () => callback('Image load is failed'));
+};
+
+// callback function
+const onImageLoaded = (error, imgElem) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const { width, height } = imgElem;
+  const sizeElem = document.querySelector('.image-size');
+
+  sizeElem.textContent = `${width} x ${height}`;
+};
+
+// examples
+// addImage('https://proza.ru/pics/2019/02/05/56.jpg', onImageLoaded);
