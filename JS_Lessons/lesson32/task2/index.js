42ae36934a9a8f842872091efd93acdfa3d610d3
amesemyta1 <amesemyta@gmail.com>
2022-07-20 18:53:54 +0300

    tsk2

--- JS_Lessons/lesson32/task2/index.js
+++ JS_Lessons/lesson32/task2/index.js
@@ -1,26 +1,31 @@
-/**
- * @param {string} userId
- * @return {promise}
- */
-export const asyncCalculator = num =>
+const getRandomNumber = (from, to) => from + Math.random() * (to - from);
+
+const request = url =>
   new Promise(resolve => {
+    const randomDelay = getRandomNumber(1000, 3000);
     setTimeout(() => {
-      console.log(`Initial value: ${num}`);
-      resolve(num);
-    }, 500);
-  })
-    .then(
-      value =>
-        new Promise(resolve => {
-          setTimeout(() => {
-            const result = value * value;
-            console.log(`Squared value: ${result}`);
-            resolve(result);
-          }, 500);
-        }),
-    )
-    .then(value => {
-      const result = value * 2;
-      console.log(`Doubled value: ${result}`);
-      return result;
-    });
+      resolve({
+        userData: {
+          name: 'Tom',
+          age: 17,
+        },
+        source: url,
+      });
+    }, randomDelay);
+  });
+
+const servers = [
+  'https://server.com/us/userId',
+  'https://server.com/eu/userId',
+  'https://server.com/au/userId',
+];
+
+export const getUserASAP = userId => {
+  const userUrls = servers.map(serverUrl => `${serverUrl}/users/${userId}`);
+
+  const requests = userUrls.map(userUrl => request(userUrl));
+
+  return Promise.race(requests);
+};
+
+getUserASAP('user-id-1').then(res => console.log(res));
