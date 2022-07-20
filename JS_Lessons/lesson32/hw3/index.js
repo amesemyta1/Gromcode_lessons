571712b971b8a50d6476d153dd8abace519d0eae
amesemyta1 <amesemyta@gmail.com>
2022-07-20 18:21:55 +0300

    ls32

---
+++ JS_Lessons/lesson32/hw3/index.js
@@ -0,0 +1,9 @@
+export const delay = time =>
+  new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, time);
+  });
+
+// delay(3000).then(() => console.log('Done'));
+// строка 'Done' должна появиться в консоли через 3 секунды
---
+++ JS_Lessons/lesson32/task1/index.js
@@ -0,0 +1,22 @@
+const getValueWithDelay = (value, delay) =>
+  new Promise(resolve => {
+    setTimeout(() => {
+      console.log(value);
+      resolve(value);
+    }, delay);
+  });
+
+const getSum = numbers =>
+  numbers.filter(value => !isNaN(value)).reduce((acc, num) => acc + Number(num), 0);
+
+export const asyncSum = (...asyncNumbers) => {
+  return Promise.all(asyncNumbers)
+    .then(numbers => getSum(numbers))
+    .catch(() => console.log('Cant calculate'));
+};
+
+const asyncNum1 = getValueWithDelay(56, 1000);
+const asyncNum2 = getValueWithDelay(66, 2000);
+const asyncNum3 = getValueWithDelay(86, 3000);
+
+asyncSum(asyncNum1, asyncNum2, asyncNum3).then(result => console.log(result));
