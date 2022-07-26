0b57abfcf4f9c47d5734addc4fabf1e296969bf7
amesemyta1 <amesemyta@gmail.com>
2022-07-26 19:34:50 +0300

    hw1

--- JS_Lessons/lesson35/hw1/index.js
+++ JS_Lessons/lesson35/hw1/index.js
@@ -1,70 +1,11 @@
-const baseUrl = 'https://62dacf26e56f6d82a76a312f.mockapi.io/api/v1/users';
-
-export function getUsersList() {
-  return fetch(baseUrl).then(response => response.json());
+export function parseUser(json) {
+  try {
+    const user = JSON.parse(json);
+    return user;
+  } catch (e) {
+    return null;
+  }
 }
 
-export function getUserById(userId) {
-  return fetch(`${baseUrl}/${userId}`).then(response => response.json());
-}
-
-export function createUser(userData) {
-  return fetch(baseUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-    },
-    body: JSON.stringify(userData),
-  });
-}
-
-export function deleteUser(userId) {
-  return fetch(`${baseUrl}/${userId}`, {
-    method: 'DELETE',
-  });
-}
-
-export function updateUser(userId, userData) {
-  return fetch(`${baseUrl}/${userId}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-    },
-    body: JSON.stringify(userData),
-  });
-}
-
-// examples
-// getUsersList().then(users => {
-//   console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
-// });
-
-// getUserById('2').then(userData => {
-//   console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
-// });
-
-// const newUserData = {
-//   email: 'cool@email.com',
-//   firstName: 'Iron',
-//   lastName: 'Man',
-//   age: 42,
-// };
-
-// createUser(newUserData).then(() => {
-//   console.log('User created');
-// });
-
-// const updatedUserData = {
-//   email: 'new@email.com',
-//   firstName: 'John',
-//   lastName: 'Doe',
-//   age: 17,
-// };
-
-// updateUser('1', updatedUserData).then(() => {
-//   console.log('User updated');
-// });
-
-// deleteUser('2').then(() => {
-//   console.log('User updated');
-// });
+// const test = parseUser(`{ "name": "John", "age": 30 }`);
+// console.log(test);
